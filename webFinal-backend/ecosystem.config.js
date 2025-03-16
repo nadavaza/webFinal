cd0df24be8e60f458e282c34e39a2632fa6f61da
nadavaza <nadavaza@gmail.com>
2025-03-17 00:39:36 +0200

    prod fixes

--- webFinal-backend/ecosystem.config.js
+++ webFinal-backend/ecosystem.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  apps : [{
-    name   : "app_d",
-    script : "./dist/src/app.js",
-    env_production : {
-      NODE_ENV: "production"
-    }
-  }]
-
-}
+  apps: [
+    {
+      name: "app_d",
+      script: "./dist/app.js",
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
